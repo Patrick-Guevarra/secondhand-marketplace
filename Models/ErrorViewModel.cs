28ab8f4384901ffb35db6168c6e649389b4040df
Patrick Guevarra <115962615+Patrick-Guevarra@users.noreply.github.com>
2025-10-18 17:06:03 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace secondhand_marketplace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
